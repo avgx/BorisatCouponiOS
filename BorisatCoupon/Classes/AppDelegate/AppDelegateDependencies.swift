800067b66c20887a58a32a91954dd9d761676c7d
German Saprykin <saprykin.h@gmail.com>
2016-10-16 16:15:10 +0700

    Added Firebase initialisation.

---
+++ BorisatCoupon/Classes/AppDelegate/AppDelegateDependencies.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegateDependencies.swift
+//  BorisatCoupon
+//
+//  Created by Herman Saprykin on 16/10/2016.
+//  Copyright © 2016 BorisatCoupon. All rights reserved.
+//
+
+import Firebase
+
+class AppStartupConfigurator {
+    
+    func configure() {
+        FIRApp.configure()
+    }
+}
